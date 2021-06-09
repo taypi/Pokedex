b3f7fcd7a9c03e972b78cdef46bc235c9b2c36ff
Tay <taypih@gmail.com>
2021-06-08 21:12:13 -0300

    Create PokemonDetail model

---
+++ app/src/main/java/com/tay/pokedex/model/PokemonDetail.kt
@@ -0,0 +1,21 @@
+package com.tay.pokedex.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PokemonDetail(
+    @field:Json(name = "id") val id: Int,
+    @field:Json(name = "name") val name: String,
+    @field:Json(name = "height") val height: Int,
+    @field:Json(name = "weight") val weight: Int,
+    @field:Json(name = "base_experience") val experience: Int,
+    @field:Json(name = "types") val types: List<TypeResponse>,
+) {
+
+    @JsonClass(generateAdapter = true)
+    data class TypeResponse(
+        @field:Json(name = "slot") val slot: Int,
+        @field:Json(name = "type") val type: Type
+    )
+}
